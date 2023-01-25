c0a8828816f6b6fb7662cea5e5816b9510a61d59
OmerFarukBATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2023-01-25 16:24:04 +0300

    main database scheme oluşturuldu.

---
+++ OnlineMuhasebeWepApi.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeWepApi.Domain.AppEntities;
+using OnlineMuhasebeWepApi.Domain.AppEntities.Identity;
+
+namespace OnlineMuhasebeWepApi.Persistance.Context
+{
+    public sealed class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<AppUserCompany> AppUserCompanies { get; set; }
+
+    }
+}
---
+++ OnlineMuhasebeWepApi.Persistance/Context/CompanyDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeWepApi.Domain.AppEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeWepApi.Persistance.Context
+{
+    public sealed class CompanyDbContext : DbContext
+    {
+        private string ConnectionString = "";
+        private readonly AppDbContext _appDbContext;
+        public CompanyDbContext(string companyId, AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            Company company = _appDbContext.Companies.Find(companyId);
+            if (company.UserId == "")
+            {
+                ConnectionString = $" Data Source={company.ServerName};" +
+                    $"Initial Catalog={company.DatabaseName};" +
+                    $"Integrated Security=True;" +
+                    $"Connect Timeout=30;" +
+                    $"Encrypt=False;" +
+                    $"TrustServerCertificate=False;" +
+                    $"ApplicationIntent=ReadWrite;" +
+                    $"MultiSubnetFailover=False";
+            }
+            else
+            {
+                ConnectionString = $" Data Source={company.ServerName};" +
+                    $"Initial Catalog={company.DatabaseName};" +
+                    $"User Id={company.UserId};" +
+                    $"Password = {company.Password};" +
+                    $"Integrated Security=True;" +
+                    $"Connect Timeout=30;" +
+                    $"Encrypt=False;" +
+                    $"TrustServerCertificate=False;" +
+                    $"ApplicationIntent=ReadWrite;" +
+                    $"MultiSubnetFailover=False";
+            }
+           
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
